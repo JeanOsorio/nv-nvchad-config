82694eef67949c59ad932a61b030aa0188030c54
Jean Osorio <josorioe@gmail.com>
2025-01-23 13:31:18 +0100

    chore: detect error on javascript projects
    with a tsconfig file inside the project

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,6 +16,39 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+    settings = (lsp == "ts_ls")
+        and {
+          javascript = {
+            validate = {
+              enable = true, -- Habilita la validación para JS
+            },
+            suggest = {
+              autoImports = true, -- Activa las auto-importaciones
+              includeAutomaticOptionalChainCompletions = true,
+            },
+            format = {
+              semicolons = "insert",
+            },
+            checkJs = true, -- Habilita la verificación de errores en JS
+          },
+          typescript = {
+            validate = {
+              enable = true,
+            },
+            format = {
+              semicolons = "insert",
+            },
+          },
+        }
+      or nil, -- Solo aplica settings si es ts_ls
+  }
+end
+
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
